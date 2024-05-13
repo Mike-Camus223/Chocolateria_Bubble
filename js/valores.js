2318ed23ae6ac586139fdb1934d3036fd9a8dead
mike camps <michaelcamposvil@gmail.com>
2024-05-13 04:16:02 -0300

    añadido js

---
+++ js/valores.js
@@ -0,0 +1,37 @@
+
+//productos//
+
+  const Productos = [
+    {id:1,
+      nombre:"Gomitas Gummy Bears",
+      precio: 1400,
+    },
+    {id:2,
+      nombre:"Trolli Candy Corns",
+      precio: 1000,
+    },
+    {id:3,
+      nombre:"Deliket Jelly Beans",
+      precio: 1600,
+    },
+    {id:4,
+      nombre:"Gomitas Sweet Tars",
+      precio: 1300,
+    },
+    {id:5,
+      nombre:"Sugus Beans",
+      precio: 2000,
+    },
+    {id:6,
+      nombre:"Flynn Paff Chupetines",
+      precio: 3000,
+    },
+  ]
+
+  for (let i = 0; i < Productos.length; i++) {
+    console.log(Productos[i])
+  }
+  
+  // for (let i = 0; i < Productos.length; i++) {
+  //   console.log("Nombre: " + P[i].nombre + ", Edad: " + personas[i].edad + ", Ciudad: " + personas[i].ciudad);
+  // }
