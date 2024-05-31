3d8159abc2745a7353353fc78ea6f9bbbdec7036
mike camps <michaelcamposvil@gmail.com>
2024-05-31 06:48:24 -0300

    filtro mejorado

--- js/index.js
+++ js/index.js
@@ -1,21 +1,25 @@
 
+// filtro selecciones //
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+
     const botonDesplegable = document.getElementById("boton-desplegable");
     const checkboxes = document.querySelectorAll(".uk-checkbox");
-    
-    checkboxes.forEach(function(checkbox) {
-      checkbox.addEventListener("change", actualizarTextoBoton);
+
+    checkboxes.forEach(function (checkbox) {
+        checkbox.addEventListener("change", actualizarTextoBoton);
     });
 
+
     function actualizarTextoBoton() {
-      const checkboxesSeleccionados = document.querySelectorAll(".uk-checkbox:checked");
-      if (checkboxesSeleccionados.length === 0) {
-        botonDesplegable.textContent = "Seleccionar Categorías";
-      } else if (checkboxesSeleccionados.length === 1) {
-        botonDesplegable.textContent = checkboxesSeleccionados[0].nextElementSibling.textContent;
-      } else {
-        botonDesplegable.textContent = checkboxesSeleccionados.length + " opciones elegidas";
-      }
+        const checkboxesSeleccionados = document.querySelectorAll(".uk-checkbox:checked");
+
+        if (checkboxesSeleccionados.length === 0) {
+            botonDesplegable.textContent = "Seleccionar Categorías";
+        } else if (checkboxesSeleccionados.length === 1) {
+            botonDesplegable.textContent = checkboxesSeleccionados[0].nextElementSibling.textContent;
+        } else {
+            botonDesplegable.textContent = checkboxesSeleccionados.length + " Selecciones";
+        }
     }
-  });
+});
