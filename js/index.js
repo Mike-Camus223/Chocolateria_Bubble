0e98ff63c94b4405b9098446a0703190b87577e4
mike camps <michaelcamposvil@gmail.com>
2024-05-31 06:32:35 -0300

    filter 10%

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    const botonDesplegable = document.getElementById("boton-desplegable");
+    const checkboxes = document.querySelectorAll(".uk-checkbox");
+    
+    checkboxes.forEach(function(checkbox) {
+      checkbox.addEventListener("change", actualizarTextoBoton);
+    });
 
-// function sumar(A,B){
-//     return A + B;
-// }
-
-// console.log(sumar(3, 4));
-
-
-
+    function actualizarTextoBoton() {
+      const checkboxesSeleccionados = document.querySelectorAll(".uk-checkbox:checked");
+      if (checkboxesSeleccionados.length === 0) {
+        botonDesplegable.textContent = "Seleccionar Categorías";
+      } else if (checkboxesSeleccionados.length === 1) {
+        botonDesplegable.textContent = checkboxesSeleccionados[0].nextElementSibling.textContent;
+      } else {
+        botonDesplegable.textContent = checkboxesSeleccionados.length + " opciones elegidas";
+      }
+    }
+  });
